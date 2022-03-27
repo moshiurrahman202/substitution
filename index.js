fa67e73cb825185f0258a77c85e9e287b50fbbcf
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-03-27 20:58:44 +0600

    substitution for age 0-9

---
+++ index.js
@@ -0,0 +1,7 @@
+var age = prompt("Enter your age :");
+age = parseInt(age);
+if(age == 1){
+    console.log(age + " When age is 0 to 1 call them Infants");
+}else if (age > 1 && age<=2){
+    console.log(age + " When age is 1 to 2 call them Toddlers");
+}
